da1f172ed7b821f811c188159c2ff06ef2016569
Ahmed Faheem <ahmedfaheemmalick@gmail.com>
2023-08-03 16:11:52 +0500

    fix all errors

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { BsCurrencyDollar } from "react-icons/bs"
-import { IoIosMore } from "react-icons/io"
-import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns"
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
-import { Stacked, Pie, Button, LineChart, SparkLine } from "../components"
+import { Stacked, Pie, Button, LineChart, SparkLine } from '../components';
 import {
   earningData,
   medicalproBranding,
@@ -12,26 +12,26 @@ import {
   dropdownData,
   SparklineAreaData,
   ecomPieChartData,
-} from "../data/dummy"
-import { useStateContext } from "../contexts/ContextProvider"
-import product9 from "../data/product9.jpg"
+} from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
 
 const DropDown = ({ currentMode }) => (
   <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
     <DropDownListComponent
       id="time"
-      fields={{ text: "Time", value: "Id" }}
-      style={{ border: "none", color: currentMode === "Dark" && "white" }}
+      fields={{ text: 'Time', value: 'Id' }}
+      style={{ border: 'none', color: currentMode === 'Dark' && 'white' }}
       value="1"
       dataSource={dropdownData}
       popupHeight="220px"
       popupWidth="120px"
     />
   </div>
-)
+);
 
 const Ecommerce = () => {
-  const { currentColor, currentMode } = useStateContext()
+  const { currentColor, currentMode } = useStateContext();
 
   return (
     <div className="mt-24">
@@ -385,7 +385,7 @@ const Ecommerce = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ecommerce
+export default Ecommerce;
